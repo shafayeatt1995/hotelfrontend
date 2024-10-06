89412e3edf837b65b063cc7d8f705c0934ab6f34
shafayeatt1995 <shafayetalanik@gmail.com>
2024-10-07 05:40:24 +0600

    updated

--- components/Home/Navbar/Login.vue
+++ components/Home/Navbar/Login.vue
@@ -1,30 +1,32 @@
 <template>
-  <div
-    class="relative"
-    v-if="isLoggedIn"
-    v-click-outside="() => (popup ? (popup = false) : '')"
-  >
-    <div class="size-10 cursor-pointer" @click="popup = !popup">
-      <nuxt-img
-        :src="$auth?.user?.avatar"
-        :alt="$auth?.user?.name"
-        class="object-cover rounded-lg w-full h-full"
-        width="80"
-        height="80"
-        format="webp"
-        loading="lazy"
-        decode="async"
-      />
+  <client-only>
+    <div
+      class="relative"
+      v-if="this.$auth.loggedIn"
+      v-click-outside="() => (popup ? (popup = false) : '')"
+    >
+      <div class="size-10 cursor-pointer" @click="popup = !popup">
+        <nuxt-img
+          :src="$auth?.user?.avatar"
+          :alt="$auth?.user?.name"
+          class="object-cover rounded-lg w-full h-full"
+          width="80"
+          height="80"
+          format="webp"
+          loading="lazy"
+          decode="async"
+        />
+      </div>
+      <transition name="fade-scale" mode="out-in">
+        <HomeNavbarProfile v-if="popup" />
+      </transition>
     </div>
-    <transition name="fade-scale" mode="out-in">
-      <HomeNavbarProfile v-if="popup" />
-    </transition>
-  </div>
-  <div class="flex justify-center" v-else>
-    <Button @click.native.prevent="$nuxt.$emit('signInPopup')">
-      Sign in
-    </Button>
-  </div>
+    <div class="flex justify-center" v-else>
+      <Button @click.native.prevent="$nuxt.$emit('signInPopup')">
+        Sign in
+      </Button>
+    </div>
+  </client-only>
 </template>
 
 <script>
@@ -35,11 +37,6 @@ export default {
     return {
       popup: false,
     };
-  },
-  computed: {
-    isLoggedIn() {
-      return this.$auth.loggedIn;
-    },
   },
   watch: {
     $route() {
