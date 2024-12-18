a787523b5aa2f141f921c6d83b8e37ae1fdd57c8
shafayeatt1995 <shafayetalanik@gmail.com>
2024-12-18 22:43:30 +0600

    amp code added

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -12,6 +12,20 @@ export default function () {
     iframe.setAttribute("style", "display:none;visibility:hidden");
     noscriptIframe.appendChild(iframe);
     document.body.appendChild(noscriptIframe);
+
+    const script = document.createElement("script");
+    script.setAttribute("async", "");
+    script.setAttribute("custom-element", "amp-auto-ads");
+    script.setAttribute(
+      "src",
+      "https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"
+    );
+    document.body.appendChild(script);
+
+    const ampAutoAds = document.createElement("amp-auto-ads");
+    ampAutoAds.setAttribute("type", "adsense");
+    ampAutoAds.setAttribute("data-ad-client", "ca-pub-4198613489910321");
+    document.body.appendChild(ampAutoAds, document.body.lastChild);
   }
 
   window.addEventListener("load", loadScript);
